b175c4d160a2af9561508f89e7fd4bbe77446c9f
paulapivat <paul.apivat@gmail.com>
2021-03-20 23:43:48 +0700

    add exploratory.R

---
+++ prototypes/exploratory.R
@@ -0,0 +1,29 @@
+library(tidyverse)
+library(stringr)
+
+factory <- read_csv("factory.csv")
+
+# extract last word from string for 'city'
+tail(strsplit('P4M6+FV San Kamphaeng, San Kamphaeng District, Chiang Mai', split = " ")[[1]],2)
+
+tail(strsplit(factory$city, split = " ")[[1]],2)
+
+factory %>%
+    mutate(
+        city2 = tail(strsplit(factory$city, split = " ")[[1]], 2)
+    )
+
+
+word('P4M6+FV San Kamphaeng, San Kamphaeng District, Chiang Mai', -2)
+
+# Extract last two words from sentence
+# https://rdrr.io/cran/stringr/man/word.html
+
+word(unlist(factory$city),start = -2, end = -1)
+
+factory %>%
+    mutate(
+        city2 = word(unlist(factory$city),start = -2, end = -1)
+    ) %>%
+    view()
+
