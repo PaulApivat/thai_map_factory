0023fac7e1ea2662a8ddf598a856f8a8a1d91fc6
paulapivat <paul.apivat@gmail.com>
2021-03-21 14:27:12 +0700

    plot factories on THAI.map

--- prototypes/exploratory.R
+++ prototypes/exploratory.R
@@ -21,13 +21,14 @@ word('P4M6+FV San Kamphaeng, San Kamphaeng District, Chiang Mai', -2)
 
 word(unlist(factory$city),start = -2, end = -1)
 
-factory %>%
+# create new column for city2
+factory1 <- factory %>%
     mutate(
         city2 = word(unlist(factory$city),start = -2, end = -1)
-    ) %>%
-    view()
+    )
 
-
+# create new data.frame to join with THAI.map
+city.map <- data.frame(Location = factory1$city2, lat = factory1$lat, long = factory1$lng)
 
 
 # Maps Package ----
@@ -37,6 +38,8 @@ world.map <- map_data("world")
 
 THAI.map <- world.map %>% filter(region == "Thailand")
 
+# visualize THAI.map + city.map
 THAI.map %>%
     ggplot() + 
-    geom_map(map = THAI.map, aes(x = long, y = lat, map_id = region), fill = "white", color = "black")
+    geom_map(map = THAI.map, aes(x = long, y = lat, map_id = region), fill = "white", color = "black") +
+    geom_point(data = city.map, aes(x = long, y = lat, color = "red"), alpha = 0.9)
